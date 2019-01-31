d4227438bbd56dfe7d0340efe6f7e6348128e43d
McCarroll, Alexandra <Alexandra.McCarroll@capco.com>
2019-01-31 10:45:56 +0000

    reder album names

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -2,20 +2,28 @@ import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import axios from 'axios';
 
+// Class based component
 class AlbumList extends Component {
+
     // initial base state
     state = {albums: []};
     
-    componentWillMount () {
-        // returns a promise
+    componentWillMount() {
+        // returns a promise & fetches data asyncronosly 
+        // ONLY use setState to update the state
         axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-            .then( response => console.log(response));
+            .then( response => this.setState({ albums: response.data }));
     }
-    //functional components need to render jsx
+    
+    renderAlbums() {
+        return this.state.albums.map(album => <Text>{album.title}</Text>);
+    }
+
     render() {
+        
         return (
             <View>
-                <Text>AlbumList</Text>
+                {this.renderAlbums()}
             </View>
         );
     }
